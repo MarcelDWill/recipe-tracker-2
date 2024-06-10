dfa00512f52222caa2ddee0d9e051fa2651e636e
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2024-06-10 12:24:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.createRoot(document.querySelector("#root"))
-  .render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
