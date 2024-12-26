de3cf62e4d1ed2464075b6a3144e08e893501d15
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2024-12-26 13:10:04 -0500

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,22 +1,31 @@
-import React from "react"
+import React from "react";
 
 function Recipe({ recipe, index, deleteRecipe }) {
-    const deleteHandler = () => deleteRecipe(index);
+  const deleteHandler = () => deleteRecipe(index);
 
+  // Check if the recipe object is defined
+  if (!recipe) {
     return (
-        <tr>
-            <td className="content_td"><p>{recipe.name}</p></td>
-            <td className="content_td"><p>{recipe.cuisine}</p></td>
-            <td className="content_td">
-                <img src={recipe.photo} alt={`Photo of ${recipe.name}`} />
-            </td>
-            <td className="content_td"><p>{recipe.ingredients.join(', ')}</p></td>
-            <td className="content_td"><p>{recipe.preparation}</p></td>
-            <td>
-                <button name="delete" onClick={deleteHandler}>Delete</button>
-            </td>
-        </tr>
+      <tr>
+        <td colSpan="6">No recipe data available</td>
+      </tr>
     );
+  }
+
+  return (
+    <tr>
+      <td className="content_td"><p>{recipe.name}</p></td>
+      <td className="content_td"><p>{recipe.cuisine}</p></td>
+      <td className="content_td">
+        <img src={recipe.photo} alt={`Photo of ${recipe.name}`} />
+      </td>
+      <td className="content_td"><p>{recipe.ingredients}</p></td> {/* Directly display ingredients */}
+      <td className="content_td"><p>{recipe.preparation}</p></td>
+      <td>
+        <button name="delete" onClick={deleteHandler}>Delete</button>
+      </td>
+    </tr>
+  );
 }
 
 export default Recipe;
