24adb43ba6db5c115530e3416b484b173e121d05
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2024-12-26 12:44:01 -0500

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,19 +1,22 @@
 import React from "react"
 
-function Recipe({recipe, index, deleteRecipe}) {
-
-    const deleteHandler = () => deleteRecipe(index)
+function Recipe({ recipe, index, deleteRecipe }) {
+    const deleteHandler = () => deleteRecipe(index);
 
     return (
         <tr>
             <td className="content_td"><p>{recipe.name}</p></td>
             <td className="content_td"><p>{recipe.cuisine}</p></td>
-            <td className="content_td"><img src={recipe.photo} /></td>
-            <td className="content_td"><p>{recipe.ingredients}</p></td>
+            <td className="content_td">
+                <img src={recipe.photo} alt={`Photo of ${recipe.name}`} />
+            </td>
+            <td className="content_td"><p>{recipe.ingredients.join(', ')}</p></td>
             <td className="content_td"><p>{recipe.preparation}</p></td>
-            <td><button name="delete" onClick={deleteHandler}>Delete</button></td>
+            <td>
+                <button name="delete" onClick={deleteHandler}>Delete</button>
+            </td>
         </tr>
-    )
+    );
 }
 
-export default Recipe
+export default Recipe;
