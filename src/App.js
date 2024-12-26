62a1409b54e9af701de743f4fac79f6a7815707c
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2024-12-26 13:09:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { useState } from "react";
 import "./App.css";
 import RecipeCreate from "./RecipeCreate";
 import RecipeList from "./RecipeList";
-import RecipeData from "./RecipeData"
+import RecipeData from "./RecipeData";
 
 function App() {
-  const [recipes, setRecipes] = useState(RecipeData);
+  const [recipes, setRecipes] = useState(RecipeData); // Initialize with RecipeData
 
-  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
-
-  const deleteRecipe = (recipeIndex) => setRecipes(recipes.filter((ignored, index) => index !== recipeIndex))
-  const createRecipe = (recipe) => setRecipes([...recipes, recipe])
+  // Function to delete a recipe
+  const deleteRecipe = (recipeIndex) => {
+    setRecipes(recipes.filter((_, index) => index !== recipeIndex));
+  };
 
   return (
     <div className="App">
       <header><h1 className="heading">Delicious Food Recipes</h1></header>
-      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
-      <RecipeCreate createRecipe={createRecipe}/>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate recipes={recipes} setRecipes={setRecipes} /> {/* Pass props here */}
     </div>
   );
 }
