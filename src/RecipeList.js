227e3b7af8654fc3ae359ee94264972df1192d04
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2024-12-26 13:10:55 -0500

    Update RecipeList.js

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,34 +1,25 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-function RecipeList({recipes, deleteRecipe}) {
-  
-  // TODO: Display the list of recipes using the structure of table that is provided.
-  // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
- const deleteRecipe = (indexToDelete) => {
-    setRecipes((currentRecipes) =>
-      currentRecipes.filter((recipe, index) => index !== indexToDelete)
-    );
-  };
+function RecipeList({ recipes, deleteRecipe }) {
   return (
-    <div className="recipe-list">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Cuisine</th>
-            <th>Photo</th>
-            <th className="area">Ingredients</th>
-            <th className="area">Preparation</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody className="content_row">
-          <EachRecipe deleteRecipe={deleteRecipe} recipes={recipes} />
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Cuisine</th>
+          <th>Photo</th>
+          <th>Ingredients</th>
+          <th>Preparation</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {recipes.map((recipe, index) => (
+          <Recipe key={index} recipe={recipe} index={index} deleteRecipe={deleteRecipe} />
+        ))}
+      </tbody>
+    </table>
   );
 }
 
